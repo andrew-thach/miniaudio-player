6f1b4272b3fcf993bf1b1efd44a6bde49f707dbd
Andrew Thach <andrew.thach123@gmail.com>
2025-05-05 18:22:24 -0700

    Converted player to high-level API for simpler starter code.

--- main.cpp
+++ main.cpp
@@ -1,63 +1,24 @@
-// Source: https://miniaud.io/docs/examples/simple_playback.html
+// Sources: 
+//     https://github.com/mackron/miniaudio?tab=readme-ov-file#examples
+//     https://miniaud.io/docs/manual/
 #include "miniaudio.h"
 
-#include <stdio.h>
+#include <iostream>
 
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
-{
-    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-    if (pDecoder == NULL) {
-        return;
-    }
-
-    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
-
-    (void)pInput;
-}
-
-int main(int argc, char** argv)
-{
+int main() {
     ma_result result;
-    ma_decoder decoder;
-    ma_device_config deviceConfig;
-    ma_device device;
+    ma_engine engine;
 
-    if (argc < 2) {
-        printf("No input file.\n");
+    result = ma_engine_init(NULL, &engine);
+    if (result != MA_SUCCESS) {
+        std::cout << "Engine failed to initialize." << std::endl;        
         return -1;
     }
 
-    result = ma_decoder_init_file(argv[1], NULL, &decoder);
-    if (result != MA_SUCCESS) {
-        printf("Could not load file: %s\n", argv[1]);
-        return -2;
-    }
+    ma_engine_play_sound(&engine, "sample-6s.wav", NULL);
 
-    deviceConfig = ma_device_config_init(ma_device_type_playback);
-    deviceConfig.playback.format   = decoder.outputFormat;
-    deviceConfig.playback.channels = decoder.outputChannels;
-    deviceConfig.sampleRate        = decoder.outputSampleRate;
-    deviceConfig.dataCallback      = data_callback;
-    deviceConfig.pUserData         = &decoder;
-
-    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
-        printf("Failed to open playback device.\n");
-        ma_decoder_uninit(&decoder);
-        return -3;
-    }
-
-    if (ma_device_start(&device) != MA_SUCCESS) {
-        printf("Failed to start playback device.\n");
-        ma_device_uninit(&device);
-        ma_decoder_uninit(&decoder);
-        return -4;
-    }
-
-    printf("Press Enter to quit...");
+    std::cout << "Press enter to quit...";
     getchar();
 
-    ma_device_uninit(&device);
-    ma_decoder_uninit(&decoder);
-
-    return 0;
+    ma_engine_uninit(&engine);
 }
