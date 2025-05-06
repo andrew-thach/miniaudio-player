cf74a407f5c1e57f4926905b68b5442f11903463
andrew-thach <andrew.thach123@gmail.com>
2025-05-05 23:45:39 -0700

    Changed which sample file to play.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
         return -1;
     }
 
-    ma_engine_play_sound(&engine, "sample-6s.wav", NULL);
+    ma_engine_play_sound(&engine, "sample-0.wav", NULL);
 
     std::cout << "Press enter to quit...";
     getchar();
